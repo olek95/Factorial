40f3bbef2ca0b17ae91535118b97621243964cb8
AleksanderSklorz <olek1995s@poczta.onet.pl>
2016-11-18 02:39:26 +0100

    Update FactorialFXMLController.java
    poprawienie wątków, blokowanie odpowiednich przycisków w danej sytuacji, przeliczanie czasu na sekundy, stworzenie metody obsługującej przycisk uruchom

--- FactorialFXMLController.java
+++ FactorialFXMLController.java
@@ -3,6 +3,8 @@ package factorial;
 import java.math.BigInteger;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -16,60 +18,77 @@ public class FactorialFXMLController implements Initializable {
     @FXML
     private TextField factorialTextField, iterationTextField, recursionTextField, iterationTimeTextField, recursionTimeTextField;
     @FXML
-    private void handleButtonAction(ActionEvent event) {
+    private void startAction(ActionEvent event) {
+        iterationTextField.setText("");
+        iterationTimeTextField.setText("");
+        recursionTextField.setText("");
+        recursionTimeTextField.setText("");
+        startButton.setDisable(true);
+        stopButton.setDisable(false);
+        iterationThread = new Thread(new Runnable() {
+            public void run(){
+                long before = System.currentTimeMillis();
+                BigInteger number = new BigInteger(factorialTextField.getText());
+                try{
+                    iterationTextField.setText(factorialIteratively(number, this).toString());
+                    iterationTimeTextField.setText(String.valueOf((double)(System.currentTimeMillis() - before)/1000) + " s");
+                    if(recursionThread.getState().equals(Thread.State.TERMINATED)){
+                        startButton.setDisable(false);
+                        stopButton.setDisable(true);
+                    }
+                }catch(InterruptedException e){
+                    Logger.getLogger(FactorialFXMLController.class.getName()).log(Level.SEVERE, null, e);
+                }
+            }
+        });
+        recursionThread = new Thread(new Runnable() {
+            public void run(){
+                long before = System.currentTimeMillis();
+                BigInteger number = new BigInteger(factorialTextField.getText());
+                try{
+                    recursionTextField.setText(factorialRecursively(number, this).toString());
+                    recursionTimeTextField.setText(String.valueOf((double)(System.currentTimeMillis() - before)/1000) + " s");
+                    if(iterationThread.getState().equals(Thread.State.TERMINATED)){
+                        startButton.setDisable(false);
+                        stopButton.setDisable(true);
+                    }
+                }catch(InterruptedException e){
+                    Logger.getLogger(FactorialFXMLController.class.getName()).log(Level.SEVERE, null, e);
+                }
+            }
+        });
+        iterationThread.start(); 
+        recursionThread.start();
     }
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        startButton.setOnAction(event -> {
-            //iterationThread = new Thread(() -> {
-                //long before = System.currentTimeMillis();
-              //  BigInteger number = new BigInteger(factorialTextField.getText());
-              //  iterationTextField.setText(factorialIteratively(number).toString());
-                //iterationTimeTextField.setText(String.valueOf(System.currentTimeMillis() - before));
-           // });
-            //recursionThread = new Thread(() -> {
-                //long before = System.currentTimeMillis();
-              // BigInteger number = new BigInteger(factorialTextField.getText());
-               // recursionTextField.setText(factorialRecursively(number).toString());
-                //recursionTimeTextField.setText(String.valueOf(System.currentTimeMillis() - before));
-           // });
-            iterationThread = new Thread(new Watek1());
-            recursionThread = new Thread(new Watek2());
-            iterationThread.start(); 
-            recursionThread.start();
+        stopButton.setDisable(true);
+        stopButton.setOnAction(event -> {
+           iterationThread.interrupt();
+           recursionThread.interrupt();
+           startButton.setDisable(false);
+           stopButton.setDisable(true);
         });
-        //stopButton.setOnAction(event -> {
-        //   iterationThread.interrupt();
-        //   recursionThread.interrupt();
-        //});
         exitButton.setOnAction(event -> {
             System.exit(0);
         });
     }    
-    private BigInteger factorialIteratively(BigInteger number){
-        System.out.println(number.toString());
+    private BigInteger factorialIteratively(BigInteger number, Runnable r) throws InterruptedException{
         BigInteger product = BigInteger.ONE;
         for(BigInteger i = BigInteger.ONE; i.compareTo(number) <= 0; i = i.add(BigInteger.ONE)){
             product = product.multiply(i);
+            synchronized(r){
+                r.wait(200);
+            }
         }
-        System.out.println(product.toString());
         return product;
     }
-    private BigInteger factorialRecursively(BigInteger number){
+    private BigInteger factorialRecursively(BigInteger number, Runnable r) throws InterruptedException{
+        synchronized(r){
+            r.wait(200);
+        }
         if(number.equals(BigInteger.ZERO)) return BigInteger.ONE;
-        return number.multiply(factorialRecursively(number.subtract(BigInteger.ONE)));
-    }
-    class Watek1 implements Runnable{
-        public void run(){
-            BigInteger number = new BigInteger(factorialTextField.getText());
-                iterationTextField.setText(factorialIteratively(number).toString());
-        }
-    }
-    class Watek2 implements Runnable{
-        public void run(){
-            BigInteger number = new BigInteger(factorialTextField.getText());
-                recursionTextField.setText(factorialRecursively(number).toString());
-        }
+        return number.multiply(factorialRecursively(number.subtract(BigInteger.ONE), r));
     }
 }
