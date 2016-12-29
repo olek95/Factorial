7704cb6d0c1e8b999f8aeff54602623fa001eee2
AleksanderSklorz <olek1995s@poczta.onet.pl>
2016-12-29 11:12:25 +0100

    Update Factorial.java

--- Factorial.java
+++ Factorial.java
@@ -14,11 +14,12 @@ public class Factorial extends Application {
         
         Scene scene = new Scene(root);
         
+        stage.setResizable(false);
+        stage.setTitle("Program porównujący metody obliczania silni");
         stage.setScene(scene);
         stage.show();
     }
     public static void main(String[] args) {
         launch(args);
     }
-    
 }
